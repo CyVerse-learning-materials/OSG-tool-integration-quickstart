c56482527db70d952da54ca1ffcde8f951d40b5a
upendrak <upendra_35@yahoo.com>
2019-10-04 16:36:34 -0700

    Modify files

--- create-tickets.sh
+++ create-tickets.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-  
 set -e
-
 mode="read"
 while getopts ":rw" opt; do
     case "$opt" in
@@ -14,12 +12,13 @@ while getopts ":rw" opt; do
     esac
 done
 shift $((OPTIND -1))
-
 path_file="$1"
-
 echo "# application/vnd.de.path-list+csv; version=1"
 for path in $(< "${path_file}"); do
     ticket=$(uuidgen | tr 'A-F' 'a-f' | tr -d '-' | fold -w 30 | head -n 1)
+    echo >&2
+    echo "Path:      $path" >&2
+    echo "Ticket ID: $ticket" >&2
     iticket create "$mode" "$path" "$ticket"
     echo "${ticket},${path}"
 done
